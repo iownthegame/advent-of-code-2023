fe1e0b393dec644768cfd8cecfe712eaf4d0a86b
Hui-Yu Lee <hlee@flexport.com>
2023-12-02 11:08:40 +0100

    Add day1 and day2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,54 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        input.forEach { line ->
+            val chars = line.toCharArray().filter { it.isDigit() }
+            sum += "${chars.first()}${chars.last()}".toInt()
+        }
+        return sum
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val digitLetters = mapOf(
+            "one" to '1',
+            "two" to '2',
+            "three" to '3',
+            "four" to '4',
+            "five" to '5',
+            "six" to '6',
+            "seven" to '7',
+            "eight" to '8',
+            "nine" to '9')
+        var sum = 0
+        input.forEach { line ->
+            val chars = line.toCharArray()
+            val newChars = mutableListOf<Char>()
+            chars.forEachIndexed { index, c ->
+                if (c.isDigit()) {
+                    newChars.add(c)
+                } else {
+                    if (index + 2 < chars.size) {
+                        val digitLetter = chars.concatToString(index)
+                        digitLetters.keys.forEach mapKey@{
+                            if (digitLetter.startsWith(it)) {
+                                newChars.add(digitLetters[it]!!)
+                                return@mapKey
+                            }
+                        }
+                    }
+                }
+            }
+            sum += "${newChars.first()}${newChars.last()}".toInt()
+        }
+        return sum
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_sample")
+    check(part1(testInput) == 142)
+
+    val testInput2 = readInput("Day01_sample2")
+    check(part2(testInput2) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
---
+++ src/Day02.kt
@@ -0,0 +1,60 @@
+fun main() {
+    fun isPossibleAnswer(bag: MutableMap<String, Int>): Boolean {
+        // 12 red cubes, 13 green cubes, and 14 blue cubes
+        if (bag.getOrDefault("red", 0) > 12) return false
+        if (bag.getOrDefault("green", 0) > 13) return false
+        if (bag.getOrDefault("blue", 0) > 14) return false
+        return true
+    }
+
+    fun part1(input: List<String>): Int {
+        // Sample input string
+        // Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+        return input.sumOf { line ->
+            val (gameString, bagsString) = line.split(": ").map { it.trim() }
+            val gameIndex = gameString.split(" ").last().toInt()
+            val bags = bagsString.split("; ").map { // bagString
+                val bag = mutableMapOf<String, Int>()
+                it.split(", ").forEach { colorString ->
+                    val (amount, color) = colorString.split(" ").map { it.trim() }
+                    bag[color] = amount.toInt()
+                }
+                bag
+            }
+
+            if (bags.all { isPossibleAnswer(it) }) gameIndex else 0
+        }
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.sumOf { line ->
+            val (gameString, bagsString) = line.split(": ").map { it.trim() }
+            val gameIndex = gameString.split(" ").last().toInt()
+            val fewestCubeInBag = mutableMapOf("red" to 0, "green" to 0, "blue" to 0)
+            bagsString.split("; ").map { // bagString
+//                val bag = mutableMapOf<String, Int>()
+                it.split(", ").forEach { colorString ->
+                    val (amount, color) = colorString.split(" ").map { it.trim() }
+//                    bag[color] = amount.toInt()
+                    fewestCubeInBag[color] = maxOf(fewestCubeInBag[color]!!, amount.toInt())
+                }
+//                bag
+            }
+
+            fewestCubeInBag["red"]!! * fewestCubeInBag["green"]!! * fewestCubeInBag["blue"]!!
+        }
+    }
+
+
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day02_sample")
+    check(part1(testInput) == 8)
+
+     val testInput2 = readInput("Day02_sample")
+     check(part2(testInput2) == 2286)
+
+    val input = readInput("Day02")
+    part1(input).println()
+    part2(input).println()
+}
